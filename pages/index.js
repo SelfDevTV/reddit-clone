6dce2648c494d63eec6a2e60d22b1582f913c8a0
SelfDevTV <SelfDevTV@gmail.com>
2020-12-26 22:30:17 +0100

    initialized the project

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Nav from '../components/nav'
+import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
     <div>
       <Nav />
       <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+        <h1 className="text-5xl text-center text-purple-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
       </div>
     </div>
-  )
+  );
 }
