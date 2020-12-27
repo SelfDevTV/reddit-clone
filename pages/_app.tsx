6b49128acedf34149e46c2970f98a95571dfd5ac
SelfDevTV <SelfDevTV@gmail.com>
2020-12-27 18:35:57 +0100

    added auth

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../styles/index.css";
+import { Provider } from "next-auth/client";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
