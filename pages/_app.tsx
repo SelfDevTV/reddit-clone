c28281a93cbe16bb9f95da16583af829482a60d5
SelfDevTV <SelfDevTV@gmail.com>
2021-01-04 16:45:54 +0100

    added loading indicator with nprogress, tidying up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import "../styles/index.css";
 import { Provider } from "next-auth/client";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+// Binding events
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   return (
